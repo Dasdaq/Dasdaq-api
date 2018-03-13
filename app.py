151598e78fe39e7a5a2f58b2d7b2b6c307bc4f00
gaojin <1019855680@qq.com>
2018-03-13 19:59:56 +0800

    增加用户排行版.

--- app.py
+++ app.py
@@ -46,7 +46,10 @@ class Dapp(Resource):
     def get(self, dapp_id):
         dapp = mongo.db.dapps.find_one(
             {'id': dapp_id}, {'_id': 0, 'address': 0})
-        return {'data': dapp}
+        if dapp:
+            return {'data': dapp}
+        else:
+            return {'error': {'code': "NOT_EXISIT", 'message': '没有找到该dapp'}}
 
 
 class DappContract(Resource):
@@ -62,35 +65,35 @@ class DappContract(Resource):
 class DappTop(Resource):
     @cache.cached(timeout=60 * 5)
     def get(self, dapp_id):
-        contract = mongo.db.top.find_one(
+        data = mongo.db.top.find_one(
             {'id': dapp_id}, {'_id': 0, 'id': 0})
-        return {'data': contract}
+        if data:
+            return {'data': sortTop(data)}
+        else:
+            return {'error': {'code': "NOT_EXISIT", 'message': '无数据'}}
 
     def post(self, ):
         # todo: 留言内容入库
         pass
 
 
-class PlaysTop(Resource):
+class User(Resource):
     @cache.cached(timeout=60 * 5)
     def get(self, address):
         a = mongo.db.usercontract.find_one({}, {'_id': 0})
-        data = []
-        for i in a[address]:
-            id, name = _getIdAndName(i['address'])
-            if id:
-                data.append({'value': i['value'], 'id': id, 'name': name})
-        # print(1)
-        return {'data': data, 'balance': 2.3, 'total': '1232324242424'}
+        if address in a:
+            data = sorted(a[address], key=lambda x: x['value'], reverse=True)
+            return {'data': data, 'balance': 2.3, 'total': '1232324242424'}
+        else:
+            return {'error': {'code': -1, 'message': u'该地址没有玩过任何游戏'}}
 
 
-def _getIdAndName(ad):
-    print(ad)
-    f = mongo.db.dapps.find_one({'address': {"$in": [ad]}})
-    if f:
-        return f['id'], f['slug']
-    else:
-        return '', ''
+class UserTop(Resource):
+    @cache.cached(timeout=60 * 5)
+    def get(self):
+        a = mongo.db.topuser.find({}, {'_id': 0})
+        data = sorted(a, key=lambda x: x['sum'], reverse=True)
+        return {'data': data}
 
 
 def getBalance(address):
@@ -102,7 +105,8 @@ api.add_resource(Dapps, '/api/dapps')
 api.add_resource(Dapp, '/api/dapps/<string:dapp_id>')
 api.add_resource(DappContract, '/api/dapps/<string:dapp_id>/contract')
 api.add_resource(DappTop, '/api/dapps/<string:dapp_id>/top')
-api.add_resource(PlaysTop, '/api/user/<string:address>')
+api.add_resource(User, '/api/user/<string:address>')
+api.add_resource(UserTop, '/api/user')
 
 
 @celery.task
@@ -110,5 +114,12 @@ def hello():
     print('hello')
 
 
+def sortTop(data):
+    win = sorted(data['data'], key=lambda x: x['value'], reverse=True)[:10]
+    loss = sorted(data['data'], key=lambda x: x['value'], reverse=False)[:10]
+    return {'win': win,
+            'loss': loss}
+
+
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000,threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=5000, threaded=True)
